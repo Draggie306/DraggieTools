dd2eaa5d05348ab9d50a6ca59e3ce14b849fdeb6
Draggie <75524491+Draggie306@users.noreply.github.com>
2022-04-15 16:08:59 +0100

    start to do it

--- DraggieTools.py
+++ DraggieTools.py
@@ -1,6 +1,32 @@
+import random
 from requests import get
-build = 0
-version = "0.0.1"
+from datetime import datetime
+
+build = 1
+version = "0.0.2"
+build_date = 1650035323
+
 
 def check_for_update():
-    x = get("")
+    print("Checking for update...")
+    current_build_version = get("https://raw.githubusercontent.com/Draggie306/DraggieTools/main/build.txt")
+    current_build_version = int(current_build_version.content)
+    if build < current_build_version:
+        update_choice = input(f"Update available! You are on version {version} which is build {build}. The new version is build {current_build_version}\n\nType 1 to download update, else not")
+        if update_choice == 1:
+            r = get("https://raw.githubusercontent.com/Draggie306/DraggieTools/main/DraggieTools.py")
+            with open('DraggieTools.py', 'wb') as f:
+                f.write(r.content)
+        else:
+            return
+    else:
+        print(f"Your tools are up to date. Running version {version} - build {build} - built at {datetime.fromtimestamp(1650035010).strftime('%Y-%m-%d %H:%M:%S')}")
+
+
+check_for_update()
+
+def main():
+    the_funny = ['Transfering sensitive files to The Criminal Network...', 'Your Computer Has Been FUCKED by the Trojan!', 'Your computer is hacked by IP: 5.172.193.104 like OS: LINUX UBUNTO and location: RUSSIAN FEDERATION', 'Problem opening the application running at executable "C:\PROGRAM FILES\RIOT CLIENT\RIOT VANGUARD\vgcsrv.exe". Would you like to scan this PC?']
+    print(random.choice(the_funny))
+
+main()
