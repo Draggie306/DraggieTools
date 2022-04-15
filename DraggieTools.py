33eb8dcaa408804ada3965b15f02736f7c6dd4ce
Draggie <75524491+Draggie306@users.noreply.github.com>
2022-04-15 17:43:13 +0100

    Build 9

--- DraggieTools.py
+++ DraggieTools.py
@@ -1,31 +1,19 @@
 import random
 from requests import get
 from datetime import datetime
-from os import path, startfile
+from os import path, startfile, mkdir
 from time import monotonic
 import shutil
+from colorama import Fore
 
-build = 8
-version = "0.1.3"
-build_date = 1650039706
+build = 9
+version = "0.1.4"
+build_date = 1650040914
 
 current_directory = path.dirname(path.realpath(__file__))
 print(f"running CLI from {current_directory}")
 
 current_build_version = int((get("https://raw.githubusercontent.com/Draggie306/DraggieTools/main/build.txt")).content)
-
-
-class BColours:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
 
 def download_update():
     r = get('https://github.com/Draggie306/DraggieTools/blob/main/dist/draggietools.exe?raw=true', stream=True)
@@ -39,16 +27,16 @@ def download_update():
             if now - last_print > 0.1:
                 pct_done = round(downloaded / file_size * 100)
                 speed = round(downloaded / (now - start) / 1024)
-                print(f'Downloading. {pct_done}% done, avg speed {speed} kbps')
+                print(Fore.LIGHTCYAN_EX + f'Downloading. {pct_done}% done, avg speed {speed} kbps')
                 last_print = now
 
 
 def check_for_update():
     print("Checking for update...")
     if build < current_build_version:
-        update_choice = input(f"{BColours.WARNING}Update available! You are on version {version} which is build {build}. The newest version is build {current_build_version}\n\n{BColours.ENDC}Type 1 to download update, else not\n\n>>> ")
+        update_choice = input(Fore.RED + f"Update available! You are on version {version} which is build {build}. The newest version is build {current_build_version}\n\nType 1 to download update, else not\n\n>>> ")
         if update_choice == "1":
-            print(f"{BColours.OKBLUE}Downloading update...")
+            print(Fore.BLUE + "Downloading update...")
             download_update()
             print("Update downloaded. Launching new version...")
             startfile(f'{current_directory}\\DraggieTools-{current_build_version}.exe')
@@ -56,7 +44,7 @@ def check_for_update():
             print("Skipping update.")
             return
     else:
-        print(f"{BColours.OKGREEN}Your tools are up to date. Running version {version} - build {build} - built at {datetime.fromtimestamp(1650035010).strftime('%Y-%m-%d %H:%M:%S')}. The server says the newest build is {current_build_version}.{BColours.ENDC}")
+        print(Fore.LIGHTGREEN_EX + f"Your tools are up to date. Running version {version} - build {build} - built at {datetime.fromtimestamp(1650035010).strftime('%Y-%m-%d %H:%M:%S')}. The server says the newest build is {current_build_version}." + Fore.WHITE)
 
 
 check_for_update()
@@ -64,7 +52,7 @@ check_for_update()
 
 def main():
     the_funny = ['Transfering sensitive files to The Criminal Network...', 'Your Computer Has Been FUCKED by the Trojan!', r'Your computer is hacked by IP: 5.172.193.104 like OS: LINUX UBUNTO and location: RUSSIAN FEDERATION', r'Problem opening the application running at executable "C:\PROGRAM FILES\RIOT CLIENT\RIOT VANGUARD\vgcsrv.exe". Would you like to scan this PC?']
-    print(f"{BColours.WARNING}{random.choice(the_funny)}{BColours.ENDC}")
+    print({random.choice(the_funny)})
     print("\n\n\nWhat would you like to do, mon frère?")
 
     def choice1():
@@ -79,6 +67,10 @@ def main():
             y = input(f"Enter the new directory. For example, '{e}'. \nNote that wherever you install me to, a new folder will be added called 'Draggie' This means that inputting the directory above will be {c}.\n\nRight click to paste!\n>>> ")
             directory = f"{current_directory}\\{path.basename(__file__)}"
             print(f"Current directory: {directory}")
+            try:
+                mkdir(f"{y}\\Draggie\\")
+            except Exception as e:
+                pass
             shutil.copyfile(directory, f"{y}\\Draggie\\{path.basename(__file__)}")
             print(f"Successfully copied file to {y}\\Draggie\\{path.basename(__file__)}")
         if x == "3":
