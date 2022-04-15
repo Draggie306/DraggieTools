2c0cb33f7970f96d6ad44fb4dc53b4ea248fc78a
Draggie <75524491+Draggie306@users.noreply.github.com>
2022-04-15 18:02:48 +0100

    Build 11

--- DraggieTools.py
+++ DraggieTools.py
@@ -4,11 +4,10 @@ from datetime import datetime
 from os import path, startfile, mkdir
 from time import monotonic
 import shutil
-from colorama import Fore
 
-build = 10
-version = "0.1.5"
-build_date = 1650040914
+build = 11
+version = "0.1.6"
+build_date = 1650041014
 
 current_directory = path.dirname(path.realpath(__file__))
 print(f"running CLI from {current_directory}")
@@ -27,16 +26,16 @@ def download_update():
             if now - last_print > 0.1:
                 pct_done = round(downloaded / file_size * 100)
                 speed = round(downloaded / (now - start) / 1024)
-                print(Fore.LIGHTCYAN_EX + f'Downloading. {pct_done}% done, avg speed {speed} kbps')
+                print(f'Downloading. {pct_done}% done, avg speed {speed} kbps')
                 last_print = now
 
 
 def check_for_update():
     print("Checking for update...")
     if build < current_build_version:
-        update_choice = input(Fore.RED + f"Update available! You are on version {version} which is build {build}. The newest version is build {current_build_version}\n\nType 1 to download update, else not\n\n>>> ")
+        update_choice = input(f"Update available! You are on version {version} which is build {build}. The newest version is build {current_build_version}\n\nType 1 to download update, else not\n\n>>> ")
         if update_choice == "1":
-            print(Fore.BLUE + "Downloading update...")
+            print("Downloading update...")
             download_update()
             print("Update downloaded. Launching new version...")
             startfile(f'{current_directory}\\DraggieTools-{current_build_version}.exe')
@@ -44,7 +43,7 @@ def check_for_update():
             print("Skipping update.")
             return
     else:
-        print(Fore.LIGHTGREEN_EX + f"Your tools are up to date. Running version {version} - build {build} - built at {datetime.fromtimestamp(1650035010).strftime('%Y-%m-%d %H:%M:%S')}. The server says the newest build is {current_build_version}." )
+        print(f"Your tools are up to date. Running version {version} - build {build} - built at {datetime.fromtimestamp(1650035010).strftime('%Y-%m-%d %H:%M:%S')}. The server says the newest build is {current_build_version}.")
 
 
 check_for_update()
@@ -52,30 +51,31 @@ check_for_update()
 
 def main():
     the_funny = ['Transfering sensitive files to The Criminal Network...', r'Your computer is hacked by IP: 5.172.193.104 like OS: LINUX UBUNTO and location: RUSSIAN FEDERATION', r'Problem opening the application running at executable "C:\PROGRAM FILES\RIOT CLIENT\RIOT VANGUARD\vgcsrv.exe". Would you like to scan this PC?']
-    print(Fore.RED + f"{random.choice(the_funny)}")
-    print(Fore.WHITE + "\n\n\nWhat would you like to do, mon frère?")
+    print(f"{random.choice(the_funny)}")
+    print("\n\n\nWhat would you like to do, mon frère?")
 
     def choice1():
         x = input("\n\n1) Install this to desktop\n2) Install this to custom directory\n3) Create shortcut on desktop\n\n>>> ")
         if x == "1":
             print("Initialising.")
-            directory = f"{current_directory}\\{path.basename(__file__)}"
+            directory = f"{current_directory}\\DraggieTools.exe"
             print(f"Current directory: {directory}")
         if x == "2":
             try:
                 e = r"C:\Program Files"
                 c = r"C:\Program Files\Draggie"
                 y = input(f"Enter the new directory. For example, '{e}'. \nNote that wherever you install me to, a new folder will be added called 'Draggie' This means that inputting the directory above will be {c}.\n\nRight click to paste!\n>>> ")
-                directory = f"{current_directory}\\{path.basename(__file__)}"
+                directory = f"{current_directory}\\DraggieTools.exe"
                 print(f"Current directory: {directory}")
                 try:
                     mkdir(f"{y}\\Draggie\\")
                 except Exception as e:
                     pass
-                shutil.copyfile(directory, f"{y}\\Draggie\\{path.basename(__file__)}")
-                print(f"Successfully copied file to {y}\\Draggie\\{path.basename(__file__)}")
+                shutil.copyfile(directory, f"{y}\\Draggie\\DraggieTools.exe")
+                print(f"Successfully copied file to {y}\\Draggie\\DraggieTools.exe")
             except Exception as e:
                 print(f"An error occured. {e}")
+                print("Please make sure that the file has not been renamed.")
                 choice1()
         if x == "3":
             print("lmao")
