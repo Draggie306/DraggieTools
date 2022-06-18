77738aa1d7af3f3e8e730410f4c18dccda1b1c75
Draggie <75524491+Draggie306@users.noreply.github.com>
2022-06-18 16:17:19 +0100

    Build 27 version 0.3.4

--- DraggieTools.py
+++ DraggieTools.py
@@ -7,20 +7,64 @@ import shutil
 import pathlib
 import sys
 import random
+import traceback
 
-build = 25
-version = "0.3.2"
-build_date = 1655559416
+
+dev_mode = True
+
+build = 27
+version = "0.3.4"
+build_date = 1655560416
+
+DraggieTools_AppData_Directory = (f"{environ['USERPROFILE']}\\AppData\\Roaming\\Draggie\\DraggieTools")
+
+if not path.exists(f"{DraggieTools_AppData_Directory}\\Logs"):
+    mkdir(f"{DraggieTools_AppData_Directory}\\Logs")
+
+if not path.exists(DraggieTools_AppData_Directory):
+    mkdir(DraggieTools_AppData_Directory)
+
+if not path.exists(f"{DraggieTools_AppData_Directory}\\UpdatedBuildsCache"):
+    mkdir(f"{DraggieTools_AppData_Directory}\\UpdatedBuildsCache")
+
+if not path.exists(f"{DraggieTools_AppData_Directory}\\SourceCode"):
+    mkdir(f"{DraggieTools_AppData_Directory}\\SourceCode")
 
 global language, language_chosen
 
+# The following are new line separated every 10 index entries
 english = ["Key error occured: ", "\n\nResorting to backup", "Downloading.", "done, average speed", "Checking for update...\n", "Downloading update...", "Running from", "What would you like to do, my friend?", "Transfering sensitive files to The Criminal Network...", "Your computer is hacked by IP: 5.172.193.104 like OS: LINUX UBUNTO and location: RUSSIAN FEDERATION", 
             r"Problem opening the application running at executable 'C:\PROGRAM FILES\RIOT CLIENT\RIOT VANGUARD\vgcsrv.exe'. Would you like to scan this PC?", "Your tools are up to date. Running version", "built at", "build", "The server says the newest build is", "Update available!", "You are on version", "The newest version is build", "Type 1 to download update, or enter to skip", "This is index 20 (defined under **language[19]**), if you see this then report as error.", 
-            "Downloading and opening up the source Python file in Explorer. To view it, open it in Notepad or you could upload it to an IDE online.", "which is build"]# Index number 21 -   ENGLISH
+            "Downloading and opening up the source Python file in Explorer. To view it, open it in Notepad or you could upload it to an IDE online.", "which is build", "Quitting..."]# Index number 22 -   ENGLISH
 
-french = ["Desolée", "\n\nRecourir à la sauvegarde", "Téléchargement.", "finir, avec vitesse moyenne", "Vérification de la mise à jour...\n", "Téléchargement de la mise à jour...", "En cours d'exécution à", "Qu'est-ce que tu voudrais faire, mon ami(e) ?", "Transfert de fichiers sensibles vers le réseau criminel...", "Votre ordinateur est piraté par IP : 5.172.193.104 comme OS : LINUX UBUNTU et emplacement : FÉDÉRATION DE RUSSIE", r"Problème d'ouverture de l'application exécutée sur l'exécutable 'C:\PROGRAM FILES\RIOT CLIENT\RIOT VANGUARD\vgcsrv.exe'. Voulez-vous scanner ce PC ?", "Vos outils sont à jour. Version en cours d'exécution", "fait à", "mini-version", "Le serveur dit que la nouvelle version est", "Mise à jour disponsible !", "Vous êtes sur le mini-version", "Le mini-version nouvelle est", "Ecrivez 1 pour télécharge la mise à jour, ou entre pour ignorer", "This is index 20 in french (defined under **language[20]**), if you see this then report as error.", "Ouverture du fichier Python source dans l'Explorateur. Pour le voir, ouvre ceci dans Bloc-notes Windows ou vous pouvez le télécharger sur un IDE en ligne.", "qui est le mini-version"]#     FRENCH
-DraggieTools_AppData_Directory = (f"{environ['USERPROFILE']}\\AppData\\Roaming\\Draggie\\DraggieTools")
+french = ["Desolée", "\n\nRecourir à la sauvegarde", "Téléchargement.", "finir, avec vitesse moyenne", "Vérification de la mise à jour...\n", "Téléchargement de la mise à jour...", "En cours d'exécution à", "Qu'est-ce que tu voudrais faire, mon ami(e) ?", "Transfert de fichiers sensibles vers le réseau criminel...", "Votre ordinateur est piraté par IP : 5.172.193.104 comme OS : LINUX UBUNTU et emplacement : FÉDÉRATION DE RUSSIE", 
+        r"Problème d'ouverture de l'application exécutée sur l'exécutable 'C:\PROGRAM FILES\RIOT CLIENT\RIOT VANGUARD\vgcsrv.exe'. Voulez-vous scanner ce PC ?", "Vos outils sont à jour. Version en cours d'exécution", "fait à", "mini-version", "Le serveur dit que la nouvelle version est", "Mise à jour disponsible !", "Vous êtes sur le mini-version", "Le mini-version nouvelle est", "Ecrivez 1 pour télécharge la mise à jour, ou entre pour ignorer", "This is index 20 in french (defined under **language[20]**), if you see this then report as error.", 
+        "Ouverture du fichier Python source dans l'Explorateur. Pour le voir, ouvre ceci dans Bloc-notes Windows ou vous pouvez le télécharger sur un IDE en ligne.", "qui est le mini-version", "En train de quitter..."]# Index number 22 - FRENCH
 
+"""
+Here are the prints for directory determining.
+"""
+
+if dev_mode:
+    import logging
+    logging.basicConfig(filename=f'{DraggieTools_AppData_Directory}\\Logs\\{version}-{build}-{time()}.log', encoding='utf-8', level=logging.DEBUG)
+    logging.debug(f'Established uplink at {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+    print(f"\n\n*-* Beta Tester Prints *-*\n\nAppData Directory: {DraggieTools_AppData_Directory}")
+    sleep(0.05)
+    print(f"Executable location: {sys.executable}")
+    sleep(0.05)
+    print(f"application_path: {path.dirname(path.abspath(__file__))}\n\nDevmode is ON, therefore enhanced logging is active.\nThe log file is located in the Roaming AppData directory")
+    sleep(0.05)
+
+"""
+End prints
+"""
+y = f"({datetime.now()}.strftime('%Y-%m-%d %H:%M:%S')"
+print(y)
+
+directory = sys.executable
+if dev_mode:
+    logging.info(f"Assigned directory to {sys.executable}")
 
 def change_language():
     global language, language_chosen
@@ -32,31 +76,30 @@ def change_language():
             language = french
             x = open(f"{DraggieTools_AppData_Directory}\\Langauge_Preference.txt", "w+")
             x.close()
+            if dev_mode:
+                logging.info(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: File at path '{DraggieTools_AppData_Directory}\\Langauge_Preference.txt' cleared")
             x = open(f"{DraggieTools_AppData_Directory}\\Langauge_Preference.txt", "w")
             x.write("French")
             x.close()
+            if dev_mode:
+                logging.info(f"({datetime.now()}.strftime('%Y-%m-%d %H:%M:%S') : File at path '{DraggieTools_AppData_Directory}\\Langauge_Preference.txt' written with 'French'")
             language_chosen = "French"
         else:
             print("Language updated to English.")
             language = english
             x = open(f"{DraggieTools_AppData_Directory}\\Langauge_Preference.txt", "w+")
             x.close()
+            if dev_mode:
+                logging.info(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: File at path '{DraggieTools_AppData_Directory}\\Langauge_Preference.txt' cleared")
+            
             x = open(f"{DraggieTools_AppData_Directory}\\Langauge_Preference.txt", "w")
             x.write("English")
             x.close()
+            if dev_mode:
+                logging.info(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: File at path '{DraggieTools_AppData_Directory}\\Langauge_Preference.txt' written with 'English'")
             language_chosen = "English"
-
-if not path.exists(DraggieTools_AppData_Directory):
-    mkdir(DraggieTools_AppData_Directory)
-
-
-if not path.exists(f"{DraggieTools_AppData_Directory}\\UpdatedBuildsCache"):
-    mkdir(f"{DraggieTools_AppData_Directory}\\UpdatedBuildsCache")
-
-
-if not path.exists(f"{DraggieTools_AppData_Directory}\\SourceCode"):
-    mkdir(f"{DraggieTools_AppData_Directory}\\SourceCode")
-
+    if dev_mode:
+        logging.info(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: Language successfully changed to {language_chosen}")
 
 if path.exists:
     try:
@@ -77,7 +120,8 @@ if path.exists:
 
 current_directory = path.dirname(path.realpath(__file__))
 print(f"{language[6]} {current_directory}")
-
+if dev_mode:
+    logging.info(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: variable currrent_directory assigned with value {current_directory}")
 
 def download_update(current_build_version):
     try:
@@ -106,10 +150,9 @@ def view_source():
     print(language[20])
     x = time()
     r = get('https://raw.githubusercontent.com/Draggie306/DraggieTools/main/DraggieTools.py')
-    with open(f'{DraggieTools_AppData_Directory}\\SourceCode\\DraggieTools-{x}.py', 'wb') as f:
+    with open(f'{DraggieTools_AppData_Directory}\\SourceCode\\DraggieTools-v{version}-{build}-{x}.py', 'wb') as f:
         f.write(r.content)
-    
-    Popen(f'explorer /select,"{DraggieTools_AppData_Directory}\\SourceCode\\DraggieTools-{x}.py"')
+    Popen(f'explorer /select,"{DraggieTools_AppData_Directory}\\SourceCode\\DraggieTools-v{version}-{build}-{x}.py"')
 
 
 def check_for_update():
@@ -147,21 +190,23 @@ try:
     check_for_update()
 except Exception as e:
     print(f"An error occured while getting the update.\n{e}\n\n")
-
+    if dev_mode:
+        logging.error(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: {traceback.format_exc()}")
 
 def main():
+    if dev_mode:
+        logging.info(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: main() subroutine executed")
     the_funny = [f'{language[8]}', f'{language[9]}', f'{language[10]}']
     print(f"{random.choice(the_funny)}")
     print(language[7])
 
     def choice1():
         if language == french:
-            x = input("\n\n1) Installez ceci sur le bureau\n2) Installez ceci dans un répertoire personnalisé\n3) Créez un raccourci sur le bureau\n4) Actualises les mises à jour\n5) Changez la langue\n6) Regarde le code source\n\n>>> ")
+            x = input("\n\n1) Installez ceci sur le bureau\n2) Installez ceci dans un répertoire personnalisé\n3) Créez un raccourci sur le bureau\n4) Actualises les mises à jour\n5) Changez la langue\n6) Regarde le code source\n8) Quitter\n\n>>> ")
         if language == english:
             x = input("\n\n1) Install this to desktop\n2) Install this to custom directory\n3) Create shortcut on desktop\n4) Refresh updates\n5) Change language\n6) View source code\n7) Quit\n\n>>> ")
         if x == "1":
             print("Initialising.")
-            directory = f"{current_directory}\\DraggieTools.exe"
             print(f"Current directory: {directory}")
             desktop = pathlib.Path.home() / 'Desktop'
             try:
@@ -170,13 +215,14 @@ def main():
             except FileNotFoundError:
                 print("Running from PYTHON file. Not executable. This should print only in the development stage.")
                 shutil.copyfile(f"{current_directory}\\DraggieTools.py", f"{desktop}\\DraggieTools.py")
-            print("I am very dumb. This will be improved later.")
+                print("I am very dumb. This will be improved later.")
+            except shutil.SameFileError:
+                print("This cannot be performed. The files are the same. Maybe it's already on the desktop!")
         if x == "2":
             try:
                 e = r"C:\Program Files"
                 c = r"C:\Program Files\Draggie"
                 y = input(f"Enter the new directory. For example, '{e}'. \nNote that wherever you install me to, a new folder will be added called 'Draggie' This means that inputting the directory above will be {c}.\n\nRight click to paste!\n>>> ")
-                directory = f"{current_directory}\\DraggieTools.exe"
                 print(f"Current directory: {directory}")
                 try:
                     mkdir(f"{y}\\Draggie\\")
@@ -184,12 +230,16 @@ def main():
                     pass
                 shutil.copyfile(directory, f"{y}\\Draggie\\DraggieTools.exe")
                 print(f"Successfully copied file to {y}\\Draggie\\DraggieTools.exe")
+                if dev_mode:
+                    logging.info(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: Copied file from '{directory}' to desired directory {y}\\Draggie\\DraggieTools.exe")
             except Exception as e:
                 print(f"An error occured. {e}")
                 print("Please make sure that the file has not been renamed.")
+                if dev_mode:
+                    logging.error(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: {traceback.format_exc()}")
                 choice1()
         if x == "3":
-            print("lmao")
+            print("Feature disabled due to a bug.")
         if x == "4":
             check_for_update()
             choice1()
@@ -212,5 +262,7 @@ def main():
 try:
     main()
 except Exception as e:
-    print(f"An unhandled exception was encountered.\n{e}\n\n")
+    print(f"An unhandled exception was encountered.\nShort: {e}\n\nLong:\n{traceback.format_exc()}\n\n")
+    if dev_mode:
+        logging.error(traceback.format_exc())
     main()
